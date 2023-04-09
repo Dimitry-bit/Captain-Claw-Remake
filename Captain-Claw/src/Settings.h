f9fd71956daa0da604e727c52f83fd73db10032f
Remo <rimo_kashafa@hotmail.com>
2023-04-09 11:01:07 +0200

    ana fngrt fy al slider bta3 al volume (Final)

--- Captain-Claw/src/Settings.h
+++ Captain-Claw/src/Settings.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#incude <iostream>
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include <iostream>
 using namespace sf;
 void SettingsSoundSlider(Sprite slider,Music music);
