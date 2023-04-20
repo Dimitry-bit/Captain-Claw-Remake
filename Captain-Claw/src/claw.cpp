a074bd2e6c3936064a0c4f6eea5619052905f47c
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2023-04-21 01:16:11 +0200

    Add: allocate game assets and load level 1

--- Captain-Claw/src/claw.cpp
+++ Captain-Claw/src/claw.cpp
@@ -3,9 +3,10 @@
 #include "claw.h"
 #include "renderer.h"
 #include "resource_manager.h"
+#include "scene_manager.h"
 
 void HandleEvent(render_context_t* renderContext);
-void UpdateAndRender(render_context_t* renderContext, sf::Time deltaTime);
+void UpdateAndRender(render_context_t* renderContext, scene_context_t* world, sf::Time deltaTime);
 
 void ClawMain()
 {
@@ -15,11 +16,20 @@ void ClawMain()
     render_context_t renderContext = {};
     RendererInit(&renderContext, WINDOW_START_WIDTH, WINDOW_START_HEIGHT);
 
+    scene_context_t world = {};
+    world.tileGridWidth = MAX_GRID_SIZE;
+    world.tileGridHeight = MAX_GRID_SIZE;
+    world.tileSize = 64;
+    entity_t* tileGrid[MAX_GRID_SIZE][MAX_GRID_SIZE] = {};
+    world.tileGrid = (entity_t**) tileGrid;
+    SceneAllocAssets(&world);
+
     sf::Clock deltaClock;
     while (window.isOpen()) {
-        UpdateAndRender(&renderContext, deltaClock.restart());
+        UpdateAndRender(&renderContext, &world, deltaClock.restart());
     }
 
+    SceneDealloc(&world);
     ResUnloadAll();
 }
 
@@ -36,10 +46,16 @@ void HandleEvent(render_context_t* renderContext)
     }
 }
 
-void UpdateAndRender(render_context_t* renderContext, sf::Time deltaTime)
+void UpdateAndRender(render_context_t* renderContext, scene_context_t* world, sf::Time deltaTime)
 {
     HandleEvent(renderContext);
 
     rWindow->clear();
+    rWindow->setView(renderContext->worldView);
+    DrawWorld(world);
+
+    rWindow->setView(renderContext->uiView);
+    // TODO(Tony): Draw UI stuff
+
     rWindow->display();
 }
--- Captain-Claw/src/scene_manager.cpp
+++ Captain-Claw/src/scene_manager.cpp
@@ -1,13 +1,50 @@
 #include <cmath>
 
 #include "scene_manager.h"
+#include "scene_serialization.h"
 #include "renderer.h"
 #include "resource_manager.h"
 #include "entity.h"
 
 void SceneAllocAssets(scene_context_t* world)
 {
+    // This is fine if we only load 1--2 levels
 
+
+    AssetPushType(ASSET_SPRITESHEET);
+    ResLoadFromFile("tilesets/LEVEL1_TILES.png", ASSET_TAG_TILE);
+    ResLoadFromFile("objects/LEVEL1_OBJECTS.png", ASSET_TAG_OBJ | ASSET_TAG_EYECANDY);
+    ResLoadFromFile("objects/GAME.png", ASSET_TAG_OBJ | ASSET_TAG_PICKUP);
+    ResLoadFromFile("objects/CHECKPOINT.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResLoadFromFile("objects/SUPERCHECKPOINT.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResLoadFromFile("objects/COINS.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_PICKUP);
+    ResLoadFromFile("objects/CRUMBLINGPEG.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResLoadFromFile("objects/EXTRALIFE.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_PICKUP);
+    ResLoadFromFile("objects/HANDS4.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResLoadFromFile("objects/MANICAL1.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResLoadFromFile("objects/TORCH.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResLoadFromFile("objects/TORSHSTAND.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResLoadFromFile("objects/CRATES.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResLoadFromFile("characters/OFFICER/OFFICER_IDLE.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResLoadFromFile("characters/SOLDIER/SOLDIER_IDLE.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    AssetPopType();
+    ResTextureLoadFromSpriteSheet("tilesets/LEVEL1_TILES.png", ASSET_TAG_TILE);
+    ResTextureLoadFromSpriteSheet("objects/LEVEL1_OBJECTS.png", ASSET_TAG_OBJ | ASSET_TAG_EYECANDY);
+    ResTextureLoadFromSpriteSheet("objects/GAME.png", ASSET_TAG_OBJ | ASSET_TAG_PICKUP);
+    ResTextureLoadFromSpriteSheet("objects/CHECKPOINT.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResTextureLoadFromSpriteSheet("objects/SUPERCHECKPOINT.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResTextureLoadFromSpriteSheet("objects/COINS.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_PICKUP);
+    ResTextureLoadFromSpriteSheet("objects/CRUMBLINGPEG.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResTextureLoadFromSpriteSheet("objects/EXTRALIFE.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_PICKUP);
+    ResTextureLoadFromSpriteSheet("objects/HANDS4.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResTextureLoadFromSpriteSheet("objects/MANICAL1.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResTextureLoadFromSpriteSheet("objects/TORCH.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResTextureLoadFromSpriteSheet("objects/TORSHSTAND.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResTextureLoadFromSpriteSheet("objects/CRATES.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION | ASSET_TAG_EYECANDY);
+    ResTextureLoadFromSpriteSheet("characters/OFFICER/OFFICER_IDLE.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+    ResTextureLoadFromSpriteSheet("characters/SOLDIER/SOLDIER_IDLE.png", ASSET_TAG_OBJ | ASSET_TAG_ANIMATION);
+
+    SceneDeserialize(world, "../resources/LEVELS/LEVEL1.cscene");
 }
 
 void SceneDealloc(scene_context_t* world)
