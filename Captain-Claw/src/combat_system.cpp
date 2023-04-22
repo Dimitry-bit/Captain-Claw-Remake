b13c05326b6b2755fed97a6b3c8f3df3594895d2
Remo <rimo_kashafa@hotmail.com>
2023-04-22 15:52:42 +0200

    animation of damage and bullet out of the screen

--- Captain-Claw/src/combat_system.cpp
+++ Captain-Claw/src/combat_system.cpp
@@ -5,6 +5,9 @@
 #include "combat_system.h"
 #include "vector_math.h"
 #include "resource_manager.h"
+#include "asset_constants.h"
+#include "animation.h"
+#include "renderer.h"
 
 using namespace sf;
 
@@ -12,12 +15,12 @@ Bullet bullets[50]; // TODO(Remo): Pull array size in separate #define
 int bulletIndex = 0;
 
 void CombatDeath(Player &actor) {
-    //actor dead
+    //death animation
 }
 
 void CombatDamage(Player &actor, int damage) {
     actor.health -= damage;
-    // TODO(Remo): Add Animation
+    CombatDamageAnimation(actor);
     if (actor.health <= 0) {
         CombatDeath(actor);
     }
@@ -27,8 +30,7 @@ void CombatAttack(Player enemy[], Player &claw, int damage, int arrSize) {
     if (Keyboard::isKeyPressed(Keyboard::LControl) || Keyboard::isKeyPressed(Keyboard::RControl))  //sword
     {
         for (int i = 0; i < arrSize; ++i) {
-            if (claw.swordCollider.getGlobalBounds().intersects(enemy[i].sprite.getGlobalBounds()))
-            {
+            if (claw.swordCollider.getGlobalBounds().intersects(enemy[i].sprite.getGlobalBounds())) {
                 CombatDamage(enemy[i], damage);
             }
         }
@@ -48,13 +50,15 @@ void BulletInit(Bullet &bullet, const Vector2f &vector, const Vector2f &directio
     bullet.isActive = true;
 }
 
-void BulletCreate(const Vector2f &position, const Vector2f &direction){
+void BulletCreate(const Vector2f &position, const Vector2f &direction) {
     BulletInit(bullets[bulletIndex], position, direction);
     bulletIndex++;
     bulletIndex %= 50;
 }
 
 void BulletsUpdate(Player enemies[], int arrSize, float deltaTime) {
+    Vector2f getCenter = rWindow->getView().getCenter(),
+            getSize = rWindow->getView().getSize();
     for (int i = 0; i < 50; ++i) {
         if (bullets[i].isActive == false) {
             continue;
@@ -70,11 +74,20 @@ void BulletsUpdate(Player enemies[], int arrSize, float deltaTime) {
             if (enemies[i].sprite.getGlobalBounds().intersects(bullets[j].sprite.getGlobalBounds())) {
                 bullets[j].isActive = false;
                 CombatDamage(enemies[i], 50);
+            } else if ( (bullets[j].sprite.getPosition().x > getSize.x / 2.0f + getCenter.x) ||
+                    (bullets[j].sprite.getPosition().x < getCenter.x - getSize.x / 2.0f) ||
+                    (bullets[j].sprite.getPosition().y > getSize.y / 2.0f + getCenter.y) ||
+                    (bullets[j].sprite.getPosition().y < getCenter.y - getSize.y / 2.0f) ) {
+                bullets[j].isActive = false;
+                //out of  screen
             }
-//          else if () {
-//              bullets[j].isActive = false;
-//              //out of  screen
-//            }
         }
     }
+}
+
+void CombatDamageAnimation(Player &deadClawPlayer) {
+    Animation deadClawAnimation;
+    AnimAnimationCreate(&ResSpriteSheetGet(CHAR_CLAW_HURT), false, 1);
+    AnimAnimatorInit(&deadClawPlayer.animator, &deadClawPlayer.sprite);
+    AnimPlay(&deadClawPlayer.animator, &deadClawAnimation);
 }
