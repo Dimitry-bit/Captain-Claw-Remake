d69e0eedb1953606046f136a35f7881cfe81832a
Pavlos <youssefpavlos@gmail.com>
2023-04-25 03:11:53 +0200

    Help from my Sensei...Thank you

--- Captain-Claw/src/Player Movement.cpp
+++ Captain-Claw/src/Player Movement.cpp
@@ -2,32 +2,58 @@
 #include "SFML/Graphics.hpp"
 #include "Player Movement.h"
 #include <math.h>
+#include <scene_manager.h>
 
 bool IsValidMove(const sf::Vector2f& pos, float groundH)
 {
     return (pos.y < groundH);
 }
+bool isValid(const scene_context_t* world, float x, float y) {
+    entity_t *tile = SceneGetTileWithPos(world, x, y);
+    if (tile != nullptr) {
+        return (tile->tile.type == TILE_GROUND || tile->tile.type == TILE_CLIMBABLE || tile->tile.type == TILE_CLEAR);
+    }
+}
 
-void MoveUnchecked(sf::Sprite& x, sf::Vector2f movement, float& deltaTime,sf::Sprite& ladder, float jumpHeight)
+void MoveUnchecked(sf::Sprite& player,float& deltaTime, float jumpHeight,scene_context_t* world)
 {
+
+
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-        movement.x += 1.0f * deltaTime;
-        x.setScale(1,0);
+        float x = player.getPosition().x + 5 * deltaTime;
+        float y = player.getPosition().y;
+        if (isValid(world,x,y))
+            player.move(5* deltaTime,0);
+        if (player.getScale().x>=0)
+            player.setScale(player.getScale().x,player.getScale().y);
+        else
+            player.setScale(player.getScale().x*-1,player.getScale().y);
+
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-        movement.x -= 1.0f * deltaTime;
-        x.setScale(-1,0);
+        float x = player.getPosition().x - 5* deltaTime;
+        float y = player.getPosition().y;
+        if(isValid(world,x,y))
+            player.move(-5.0f * deltaTime,0);
+        if (player.getScale().x>=0)
+            player.setScale(player.getScale().x*-1,player.getScale().y);
+        else
+            player.setScale(player.getScale().x,player.getScale().y);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && x.getGlobalBounds().intersects(ladder.getGlobalBounds())){
-        movement.y -= 1.0f * deltaTime;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+        float x = player.getPosition().x;
+        float y = player.getPosition().y - 5* deltaTime;
+        if(isValid(world,x,y))
+           player.move(0,- 5.0f * deltaTime);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && x.getGlobalBounds().intersects(ladder.getGlobalBounds())){
-        movement.y += 1.0f * deltaTime;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+        float x = player.getPosition().x;
+        float y = player.getPosition().y + 5* deltaTime;
+        if(isValid(world,x,y))
+            player.move(0,5.0f * deltaTime);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-        movement.y= -sqrtf(2.0f * 981.0f * jumpHeight);
-        movement.y += 981.0f * deltaTime;
+        player.move(0,-sqrtf(5.0f * 981.0f * jumpHeight));
+        player.move(0,981.0f * deltaTime);
     }
-
-    x.move(movement);
 }
