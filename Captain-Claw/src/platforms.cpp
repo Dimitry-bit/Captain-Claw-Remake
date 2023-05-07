beaf8998b1264aa673ce1babf0bd0d98a801d946
Pavlos <youssefpavlos@gmail.com>
2023-05-07 23:51:52 +0300

    3mlt fucky wucky fy al moving platform

--- Captain-Claw/src/platforms.cpp
+++ Captain-Claw/src/platforms.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
+#include "entity.h"
+#include "platforms.h"
 
-
-void PlatformsMovement(sf::Sprite platform,float deltaTime,float start,float end){
-    if(platform.getPosition().x == start)
-    {
-        platform.move(5*deltaTime, 0);
-    }
-    if(platform.getPosition().x + platform.getLocalBounds().width== end)
-    {
-        platform.move(-5*deltaTime, 0);
-    }
+void PlatformMoving(c_platform_t *platform,float deltaTime,sf::Transformable *transform){
+    float leftBound=platform->starting.x - platform->a.x;
+    float rightBound=platform->starting.x  + platform->b.x;
+    if(transform->getPosition().x >= rightBound)
+        transform->move(-5*deltaTime,0);
+    else if(transform->getPosition().x <= leftBound)
+        transform->move(5*deltaTime,0);
 }
