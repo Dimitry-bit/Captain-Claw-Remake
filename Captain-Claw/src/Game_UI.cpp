5bce8a58ad7f84db4e602bb6be4f723dcbb7e569
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2023-05-09 01:45:06 +0300

    Fix: UI_PISTOL origin

--- Captain-Claw/src/Game_UI.cpp
+++ Captain-Claw/src/Game_UI.cpp
@@ -5,9 +5,9 @@
 #include "resource_manager.h"
 #include "animation.h"
 #include  "asset_constants.h"
-#include "entity.h"
 #include "ecs.h"
 #include"entity_components.h"
+
 Animator animator_Treasure;
 Animator animator_health;
 Animator animator_ammo;
@@ -20,6 +20,7 @@ sf::Text Treasure_UI_Text;
 sf::Text Health_UI_Text;
 sf::Text Ammo_UI_Text;
 sf::Text Lives_UI_Text;
+
 void UI_Treasure_init()
 {
     ResFontGet("JetBrainsMono-Regular.ttf");
@@ -27,20 +28,22 @@ void UI_Treasure_init()
     Treasure_UI_Text.setCharacterSize(20);
     Treasure_UI_Text.setFillColor(sf::Color::Yellow);
     Treasure_UI_Text.setStyle(sf::Text::Bold);
-    Animation TreasureAnimation =AnimAnimationCreate(&ResSpriteSheetGet(UI_TREASURECHEST));
-    AnimAnimatorInit( &animator_Treasure,&Treasure);
-    AnimPlay (&animator_Treasure,&TreasureAnimation);
-    Treasure.setScale(2,2);
+    Animation TreasureAnimation = AnimAnimationCreate(&ResSpriteSheetGet(UI_TREASURECHEST));
+    AnimAnimatorInit(&animator_Treasure, &Treasure);
+    AnimPlay(&animator_Treasure, &TreasureAnimation);
+    Treasure.setScale(2, 2);
 }
-void UI_Treasure_Update(ECS*ecs,unsigned long long player_id)
+
+void UI_Treasure_Update(ECS* ecs, unsigned long long player_id)
 {
-    c_inventory_t * inventory = (c_inventory_t*) ECSGet(ecs,player_id,C_INVENTORY);
-    Treasure.setPosition(20,30);
+    c_inventory_t* inventory = (c_inventory_t*) ECSGet(ecs, player_id, C_INVENTORY);
+    Treasure.setPosition(20, 30);
     Treasure_UI_Text.setString(std::to_string(inventory->gold_counter));
-    Treasure_UI_Text.setPosition(Treasure.getPosition().x+80,Treasure.getPosition().y+10);
+    Treasure_UI_Text.setPosition(Treasure.getPosition().x + 80, Treasure.getPosition().y + 10);
     rWindow->draw(Treasure);
     rWindow->draw(Treasure_UI_Text);
 }
+
 void UI_Health_init()
 {
     ResFontGet("JetBrainsMono-Regular.ttf");
@@ -48,20 +51,22 @@ void UI_Health_init()
     Health_UI_Text.setCharacterSize(20);
     Health_UI_Text.setFillColor(sf::Color::Black);
     Health_UI_Text.setStyle(sf::Text::Bold);
-    Animation HealthAnimation =AnimAnimationCreate(&ResSpriteSheetGet(UI_HEALTHHEART));
-    AnimAnimatorInit( &animator_health,&health);
-    AnimPlay (&animator_health,&HealthAnimation);
-    health.setScale(2,2);
+    Animation HealthAnimation = AnimAnimationCreate(&ResSpriteSheetGet(UI_HEALTHHEART));
+    AnimAnimatorInit(&animator_health, &health);
+    AnimPlay(&animator_health, &HealthAnimation);
+    health.setScale(2, 2);
 }
-void UI_Health_Update(ECS*ecs,unsigned long long player_id)
+
+void UI_Health_Update(ECS* ecs, unsigned long long player_id)
 {
-    c_damageable_t * Health = (c_damageable_t*) ECSGet(ecs,player_id,C_DAMAGEABLE);
-    health.setPosition(rWindow->getSize().x-100,30);
+    c_damageable_t* Health = (c_damageable_t*) ECSGet(ecs, player_id, C_DAMAGEABLE);
+    health.setPosition(rWindow->getSize().x - 100, 30);
     Health_UI_Text.setString(std::to_string(Health->health));
-    Health_UI_Text.setPosition(health.getPosition().x+(-40),health.getPosition().y+10);
+    Health_UI_Text.setPosition(health.getPosition().x + (-40), health.getPosition().y + 10);
     rWindow->draw(health);
     rWindow->draw(Health_UI_Text);
 }
+
 void UI_ammo_init()
 {
     ResFontGet("JetBrainsMono-Regular.ttf");
@@ -69,21 +74,24 @@ void UI_ammo_init()
     Ammo_UI_Text.setCharacterSize(15);
     Ammo_UI_Text.setFillColor(sf::Color::Black);
     Ammo_UI_Text.setStyle(sf::Text::Bold);
-    Animation ammoAnimation =AnimAnimationCreate(&ResSpriteSheetGet(UI_PISTOL));
-    AnimAnimatorInit( &animator_ammo,&Ammo);
-    AnimPlay (&animator_ammo,&ammoAnimation);
-    Ammo.setScale(2,2);
+    Animation ammoAnimation = AnimAnimationCreate(&ResSpriteSheetGet(UI_PISTOL));
+    AnimAnimatorInit(&animator_ammo, &Ammo);
+    animator_ammo.adjustOrigin = false;
+    AnimPlay(&animator_ammo, &ammoAnimation);
+    Ammo.setScale(2, 2);
 }
-void UI_ammo_Update(ECS*ecs,unsigned long long player_id)
+
+void UI_ammo_Update(ECS* ecs, unsigned long long player_id)
 {
-    c_inventory_t * ammo = (c_inventory_t*) ECSGet(ecs,player_id,C_INVENTORY);
-    Ammo.setPosition(rWindow->getSize().x-100,105);
+    c_inventory_t* ammo = (c_inventory_t*) ECSGet(ecs, player_id, C_INVENTORY);
+    Ammo.setPosition(rWindow->getSize().x - 50, 115);
     Ammo_UI_Text.setString(std::to_string(ammo->ammo_pistol));
-    Ammo_UI_Text.setPosition(Ammo.getPosition().x+(-40), Ammo.getPosition().y-10);
+    Ammo_UI_Text.setPosition(Ammo.getPosition().x + (-95), Ammo.getPosition().y - 25);
+    Ammo.setOrigin(Ammo.getTextureRect().width, Ammo.getTextureRect().height);
     rWindow->draw(Ammo);
     rWindow->draw(Ammo_UI_Text);
-
 }
+
 void UI_lives_init()
 {
     ResFontGet("JetBrainsMono-Regular.ttf");
@@ -91,17 +99,18 @@ void UI_lives_init()
     Lives_UI_Text.setCharacterSize(20);
     Lives_UI_Text.setFillColor(sf::Color::Black);
     Lives_UI_Text.setStyle(sf::Text::Bold);
-    Animation LivesAnimation =AnimAnimationCreate(&ResSpriteSheetGet( UI_LIVESHEAD));
-    AnimAnimatorInit( &animator_lives,&Lives);
-    AnimPlay (&animator_lives,&LivesAnimation);
-    Lives.setScale(2,2);
+    Animation LivesAnimation = AnimAnimationCreate(&ResSpriteSheetGet(UI_LIVESHEAD));
+    AnimAnimatorInit(&animator_lives, &Lives);
+    AnimPlay(&animator_lives, &LivesAnimation);
+    Lives.setScale(2, 2);
 }
-void UI_lives_Update(ECS*ecs,unsigned long long player_id)
+
+void UI_lives_Update(ECS* ecs, unsigned long long player_id)
 {
-    c_damageable_t * lives = (c_damageable_t*) ECSGet(ecs,player_id,C_DAMAGEABLE);
+    c_damageable_t* lives = (c_damageable_t*) ECSGet(ecs, player_id, C_DAMAGEABLE);
     Lives_UI_Text.setString(std::to_string(lives->lives));
-    Lives.setPosition(rWindow->getSize().x-90,130);
-    Lives_UI_Text.setPosition(Lives.getPosition().x+(-30) , Lives.getPosition().y+10);
+    Lives.setPosition(rWindow->getSize().x - 90, 130);
+    Lives_UI_Text.setPosition(Lives.getPosition().x + (-30), Lives.getPosition().y + 10);
     rWindow->draw(Lives);
     rWindow->draw(Lives_UI_Text);
 }
--- Captain-Claw/src/Game_UI.h
+++ Captain-Claw/src/Game_UI.h
@@ -6,16 +6,12 @@
 #include "SFML/Graphics.hpp"
 #include "pickup_system.h"
 #include "resource_manager.h"
+
 void UI_Treasure_init();
-void UI_Treasure_Update(ECS*ecs,unsigned long long player_id);
+void UI_Treasure_Update(ECS* ecs, unsigned long long player_id);
 void UI_Health_init();
-void UI_Health_Update(ECS*ecs,unsigned long long player_id);
+void UI_Health_Update(ECS* ecs, unsigned long long player_id);
 void UI_ammo_init();
-void UI_ammo_Update(ECS*ecs,unsigned long long player_id);
+void UI_ammo_Update(ECS* ecs, unsigned long long player_id);
 void UI_lives_init();
-void UI_lives_Update(ECS*ecs,unsigned long long player_id);
-
-
-
-
-
+void UI_lives_Update(ECS* ecs, unsigned long long player_id);
